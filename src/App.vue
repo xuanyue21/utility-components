711b040b905fb46fd28830c6d35e4d3d9fe6db21
zhaikefeng <zhaikefeng21@163.com>
2021-03-20 14:02:41 +0800

    feat:初始化vue项目

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <!--
  * @Author: 翟珂峰
- * @LastEditTime: 2021-03-20 13:46:01
+ * @LastEditTime: 2021-03-20 14:01:34
  * @Description: 
 -->
 <template>
   <div id="app">
-      哈哈哈
+      哈哈
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
